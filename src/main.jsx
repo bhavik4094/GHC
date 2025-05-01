0e203944f1dc0659e24b9d5fc83945eda2be6537
Bhavik <bhavik4094@gmail.com>
2025-05-01 13:25:33 +0530

    Create normal structure of the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '../src/assets/css/main.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "swiper/swiper-bundle.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
